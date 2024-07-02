a0fcf2ca3ac438ce90952eb22ff4e1f7132a5cb5
CaioGBDesign <caio.gbdesign@gmail.com>
2024-07-01 23:22:21 -0300

    alteração em perfil e fundos filmes

--- src/components/fundotitulos/index.jsx
+++ src/components/fundotitulos/index.jsx
@@ -3,7 +3,7 @@ import styles from "./index.module.scss";
 
 const FundoTitulos = ({ capaAssistidos, tituloAssistidos }) => {
     const [scrollY, setScrollY] = useState(0);
-    const [imgSize, setImgSize] = useState(100); // Inicia com tamanho mínimo
+    const [imgTop, setImgTop] = useState(0); // Inicia com posição top 0px
 
     useEffect(() => {
         let requestId;
@@ -11,16 +11,17 @@ const FundoTitulos = ({ capaAssistidos, tituloAssistidos }) => {
         const handleScroll = () => {
             requestId = requestAnimationFrame(() => {
                 setScrollY(window.scrollY);
-                updateImgSize();
+                updateImgPosition();
             });
         };
 
-        const updateImgSize = () => {
-            const minSize = 100; // 100%
-            const maxSize = 110; // 110%
+        const updateImgPosition = () => {
+            // Ajuste o valor conforme necessário para a posição top desejada
+            const maxTop = -50; // Valor máximo do top em pixels
+            const minTop = 0; // Valor mínimo do top em pixels
             const scrollPercentage = Math.min(scrollY / 200, 1); // Limita a 1 após 200px de scroll
-            const newSize = minSize + (maxSize - minSize) * scrollPercentage;
-            setImgSize(newSize);
+            const newTop = minTop + (maxTop - minTop) * scrollPercentage;
+            setImgTop(newTop);
         };
 
         window.addEventListener('scroll', handleScroll);
@@ -32,13 +33,16 @@ const FundoTitulos = ({ capaAssistidos, tituloAssistidos }) => {
     }, [scrollY]);
 
     const imgStyle = {
-        width: `${imgSize}%`,
-        transition: 'width 0.2s ease', // Adiciona transição apenas para a largura da imagem
+        position: 'absolute',
+        top: `${imgTop}px`, // Define o valor do top conforme o estado imgTop
+        transition: 'top 0.2s ease', // Adiciona transição apenas para o top da imagem
     };
 
     return (
-        <div className={styles.capaAssistidos}>
-            <img src={capaAssistidos} alt={tituloAssistidos} style={imgStyle} />
+        <div className={styles.contCapa}>
+            <div className={styles.capaAssistidos} style={{ position: 'relative' }}>
+                <img src={capaAssistidos} alt={tituloAssistidos} style={imgStyle} />
+            </div>
         </div>
     );
 }

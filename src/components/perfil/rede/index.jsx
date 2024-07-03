7eee6609ca9dbb0887debac1b7547b68fee49702
CaioGBDesign <caio.gbdesign@gmail.com>
2024-07-03 18:50:41 -0300

    alteração rede perfil

--- src/components/perfil/rede/index.jsx
+++ src/components/perfil/rede/index.jsx
@@ -2,15 +2,15 @@ import styles from "./index.module.scss";
 import Link from "next/link";
 
 const Rede = ({ linkRede, titulo, valor, iconePerfil }) => {
-    return (
-        <Link href={linkRede} style={{ textDecoration: 'none' }}>
-            <div className={styles.rede}>
-                <img src={iconePerfil} />
-                <span>{titulo}</span>
-                <span>{valor}</span>
-            </div>
-        </Link>
-    );
-}
+  return (
+    <Link href={linkRede} style={{ textDecoration: "none" }}>
+      <div className={styles.rede}>
+        <img src={iconePerfil} />
+        <h1>{titulo}</h1>
+        <p>{valor}</p>
+      </div>
+    </Link>
+  );
+};
 
 export default Rede;

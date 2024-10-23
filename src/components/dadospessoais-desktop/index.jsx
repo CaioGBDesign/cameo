f7fca4d9d5aaa69073c2fe7b574cbecd1e9c9ffe
Caio Goulart <caio.gbdesign@gmail.com>
2024-10-23 01:30:25 -0300

    criado modal de dados pessoais

---
+++ src/components/dadospessoais-desktop/index.jsx
@@ -0,0 +1,168 @@
+import styles from "./index.module.scss";
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "@/contexts/auth";
+import FotoPrincipal from "@/components/perfil/fotoPrincipal";
+import NomeUsuario from "@/components/perfil/nomeUsuario";
+import Handle from "@/components/perfil/handle";
+import BotaoSecundario from "@/components/botoes/secundarios";
+
+const DadosPessoaisModalDesktop = ({ closeModal, isClosing }) => {
+  const { user, storageUser, setUser, logout } = useContext(AuthContext);
+  const [nome, setNome] = useState(user ? user.nome : "");
+  const [email, setEmail] = useState(user ? user.email : "");
+  const [handle, setHandle] = useState(user ? user.handle : "");
+  const [genero, setGenero] = useState(user ? user.genero : "");
+  const [estilo, setEstilo] = useState(user ? user.estilo : "");
+  const [alteracoesPendentes, setAlteracoesPendentes] = useState(false);
+
+  const selectGenero = [
+    { value: "Mulher", label: "Mulher" },
+    { value: "Homem", label: "Homem" },
+    { value: "Não binário", label: "Não binário" },
+    { value: "Outro", label: "Outro" },
+    { value: "Prefiro não dizer", label: "Prefiro não dizer" },
+  ];
+
+  const estiloCinefilo = [
+    { value: "PotterHead", label: "PotterHead" },
+    { value: "MeioSangue", label: "Meio Sangue" },
+    // Adicione outros estilos aqui
+  ];
+
+  const handleRedefinirSenha = async () => {
+    const auth = getAuth();
+    const actionCodeSettings = {
+      url: "https://www.cameo.fun/redefinir-senha", // Substitua pela URL da sua página de redefinição de senha
+      handleCodeInApp: true, // Isso é importante para garantir que o link seja tratado pela sua aplicação
+    };
+
+    try {
+      await sendPasswordResetEmail(auth, email, actionCodeSettings);
+      setModalSentVisible(true);
+    } catch (error) {
+      console.error("Erro ao enviar e-mail de redefinição:", error);
+    }
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("Tentando atualizar:", { nome, genero, estilo });
+
+    const updatedData = {};
+    if (nome !== user.nome) updatedData.nome = nome;
+    if (genero !== user.genero) updatedData.genero = genero;
+    if (estilo !== user.estilo) updatedData.estilo = estilo;
+
+    if (Object.keys(updatedData).length > 0) {
+      const docRef = doc(db, "users", user.uid);
+      try {
+        await updateDoc(docRef, updatedData);
+
+        let updatedUser = {
+          ...user,
+          ...updatedData,
+        };
+
+        setUser(updatedUser);
+        storageUser(updatedUser);
+        setAlteracoesPendentes(false);
+        console.log("Dados atualizados com sucesso.");
+      } catch (error) {
+        console.error("Erro ao atualizar dados:", error);
+      }
+    } else {
+      console.log("Nenhuma alteração detectada.");
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      setAlteracoesPendentes(
+        nome !== user.nome || genero !== user.genero || estilo !== user.estilo
+      );
+    }
+  }, [nome, genero, estilo, user]);
+
+  return (
+    <div className={`${styles.modal} ${isClosing ? styles.close : ""}`}>
+      <div className={styles.fecharDesktop}>
+        <button onClick={closeModal}>
+          <img src="/icones/fechar-filtros.svg" />
+        </button>
+      </div>
+      <div className={styles.contModal}>
+        <div className={styles.tituloFiltros}>
+          <h2>Dados pessoais</h2>
+        </div>
+
+        <div className={styles.FotoNomeHandle}>
+          <FotoPrincipal></FotoPrincipal>
+          <div className={styles.NomeHandle}>
+            <NomeUsuario></NomeUsuario>
+            <Handle></Handle>
+          </div>
+        </div>
+
+        <div className={styles.contInfos}>
+          <div className={styles.separador}>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formDadosPessoais}>
+                <input
+                  type="text"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                  placeholder="Nome"
+                />
+
+                <div className={styles.inputDisable}>
+                  <p>{handle}</p>
+                </div>
+
+                <div className={styles.inputDisable}>
+                  <p>{email}</p>
+                </div>
+              </div>
+
+              <div className={styles.baseSalvar}>
+                <button type="submit">Salvar alterações</button>
+              </div>
+            </form>
+          </div>
+
+          <div className={styles.separador}>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formDadosPessoais}>
+                <select
+                  id="selectGenero"
+                  value={genero}
+                  onChange={(e) => setGenero(e.target.value)}
+                >
+                  <option value="">Gênero</option>
+                  {selectGenero.map((item, index) => (
+                    <option key={`${item.value}-${index}`} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
+                </select>
+
+                <div className={styles.sair}>
+                  <button type="button" onClick={logout}>
+                    Sair
+                    <img src="/icones/sair.svg" />
+                  </button>
+                </div>
+              </div>
+
+              <div className={styles.baseSalvar}>
+                <button type="submit">Salvar alterações</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DadosPessoaisModalDesktop;
--- src/components/perfil/miniatura/index.jsx
+++ src/components/perfil/miniatura/index.jsx
@@ -1,27 +1,69 @@
 import styles from "./index.module.scss";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "@/contexts/auth";
+import { useIsMobile } from "@/components/DeviceProvider";
 import Image from "next/image";
+import DadosPessoaisModalDesktop from "@/components/dadospessoais-desktop";
 
 const FotoPerfil = ({ href = "/perfil", children }) => {
   const { user } = useContext(AuthContext);
   const avatarUrl = user && user.avatarUrl;
   const nomeUsuario = user && user.nome ? user.nome : "Nome de Usuário";
 
+  const isMobile = useIsMobile();
+
+  const [isModalOpen, setModalOpen] = useState(false); // Estado para controlar o modal
+  const [isClosing, setIsClosing] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true); // Abre o modal
+  };
+
+  const closeModal = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setModalOpen(false); // Fecha após a animação
+    }, 300); // Duração da animação
+  };
+
   return (
-    <Link href={href} className={styles["foto-perfil"]}>
-      <div className={styles.fotoPerfilMiniatura}>
-        <Image
-          src={avatarUrl || "/usuario/usuario.jpg"}
-          alt={nomeUsuario}
-          layout="fill" // Usa o layout fill
-          objectFit="cover" // Ajusta a imagem para cobrir o contêiner
-          quality={50} // Ajuste a qualidade se necessário
+    <>
+      {isMobile ? (
+        <Link href={href} className={styles["foto-perfil"]}>
+          <div className={styles.fotoPerfilMiniatura}>
+            <Image
+              src={avatarUrl || "/usuario/usuario.jpg"}
+              alt={nomeUsuario}
+              layout="fill"
+              objectFit="cover"
+              quality={50}
+            />
+          </div>
+          {children}
+        </Link>
+      ) : (
+        <div onClick={openModal} className={styles["foto-perfil"]}>
+          <div className={styles.fotoPerfilMiniatura}>
+            <Image
+              src={avatarUrl || "/usuario/usuario.jpg"}
+              alt={nomeUsuario}
+              layout="fill"
+              objectFit="cover"
+              quality={50}
+            />
+          </div>
+          {children}
+        </div>
+      )}
+
+      {isModalOpen && (
+        <DadosPessoaisModalDesktop
+          closeModal={closeModal}
+          isClosing={isClosing}
         />
-      </div>
-      {children}
-    </Link>
+      )}
+    </>
   );
 };
 

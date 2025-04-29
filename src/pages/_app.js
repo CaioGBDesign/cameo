9ed85226d618393ccc7e7eb3d4f4b317ba3a0bb6
Caio <caio.gbdesign@gmail.com>
2025-04-28 22:55:32 -0300

    teste de identificação de dispositivo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { Nunito } from "next/font/google";
 import AuthProvider from "@/contexts/auth";
 import { MantineProvider } from "@mantine/core";
 import { ToastContainer } from "react-toastify"; // Importa o ToastContainer
-import { DeviceProvider } from "@/components/DeviceProvider";
+import { DeviceProvider } from "@/components/DeviceContext";
 import "react-toastify/dist/ReactToastify.css"; // Importa o CSS do Toastify
 
 const nunito = Nunito({ subsets: ["latin"] });
